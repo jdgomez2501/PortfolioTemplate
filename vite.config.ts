cf984f4e9734704bc68f916bee6e3a526dee85a9
jdgomez2501 <127771352+jdgomez2501@users.noreply.github.com>
2026-01-21 11:50:53 +0100

    Redine base on vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // <-- Esto arregla los paths para GitHub Pages
+  base: mode === "development" ? "/" : "/PortfolioTemplate/", // <--- reemplaza con el nombre exacto de tu repo
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    // Descomenta la siguiente línea solo si quieres usar componentTagger en dev
+    // mode === 'development' && componentTagger(),
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
