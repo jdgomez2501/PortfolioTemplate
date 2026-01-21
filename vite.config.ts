050c53f1c80f205edc72514f66f0835f96b0fc11
jdgomez2501 <127771352+jdgomez2501@users.noreply.github.com>
2026-01-21 11:26:00 +0100

    Fix base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  plugins: [react()],
-  base: command === "build" ? "/PortfolioTemplate/" : "/",  
+export default defineConfig(({ mode }) => ({
+  base: mode === 'development' ? '/' : '/PortfolioTemplate/',
+
+  server: {
+    host: "::",
+    port: 8080,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
